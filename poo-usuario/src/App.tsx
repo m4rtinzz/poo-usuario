29162a485a9c0949c2f00a6e262a7d52ca16094b
m4rtinzz <contatotin41@gmail.com>
2025-07-30 10:28:14 -0300

    1 usuário adicionado

--- poo-usuario/src/App.tsx
+++ poo-usuario/src/App.tsx
@@ -5,10 +5,14 @@ function App()
 {
 
   const u1 = new Usuario('João da Silva', 37);
+  const u2 = new Usuario('Maria Oliveira', 25);
+
+  
 
   return (
     <>
       <p>{u1.apresentar()}</p>
+      <p>{u2.apresentar()}</p>
     </>
   )
 }
