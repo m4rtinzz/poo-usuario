e7e88b9e463f7f8c27a5a0e34ea99e25171c60ce
m4rtinzz <contatotin41@gmail.com>
2025-07-30 10:46:14 -0300

    Protegendo os atributos

--- poo-usuario/src/App.tsx
+++ poo-usuario/src/App.tsx
@@ -7,7 +7,7 @@ function App()
   const u1 = new Usuario('João da Silva', 37);
   const u2 = new Usuario('Maria Oliveira', 25);
 
-  
+  u1.nome = 'João da Silva Teste erro'; // This line will cause an error because nome is private
 
   return (
     <>
